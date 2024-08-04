34f19e6970c8e7ae70899af225717c72f90d90bc
Amos Adebisi <adebisiayodeji20@gmail.com>
2024-08-04 14:46:24 +0100

    shopping cart model

--- obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e72c09c46832d67d80287a7c137fa73377bbee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c031ee055313e2d36158d40eb6499e0dabcdd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
